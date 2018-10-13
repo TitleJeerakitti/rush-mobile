4fd2fda933ae2f58ceb2dfde7f5c9f352276ef6b
TitleJeerakitti <title.jeerakitti@gmail.com>
2018-10-13 15:48:40 +0700

    Refactor Swiper Code

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,71 +1,20 @@
 import React from 'react';
-import { View, ScrollView, Text, Image, Dimensions } from 'react-native';
-import Swiper from 'react-native-swiper';
+import { ScrollView, Text, } from 'react-native';
+import { Slick, SlickItem } from './common';
+
 
 export default class HomeScreen extends React.Component {
     render() {
-        const { width } = Dimensions.get('window');
-        const imageHeight = Math.round((width * 5) / 9); // 1080 x 600
         return (
             <ScrollView style={{ flex: 1 }}>
-                <Swiper 
-                    style={styles.wrapper} 
-                    autoplay 
-                    showsPagination 
-                    dotColor='transparent' 
-                    dotStyle={{ borderWidth: 1, borderColor: 'white', }} 
-                    activeDotColor='white' 
-                    height={imageHeight}
-                >
-                    <View style={styles.slide1}>
-                        <Image 
-                            resizeMode={'cover'}
-                            style={{ width: '100%', height: imageHeight }} 
-                            source={require('../images/promotion_mockup.png')} 
-                        />
-                    </View>
-                    <View style={styles.slide2}>
-                        <Image 
-                            resizeMode={'cover'}
-                            style={{ width: '100%', height: imageHeight }} 
-                            source={require('../images/promotion_2.png')} 
-                        />
-                    </View>
-                    <View style={styles.slide3}>
-                        <Image 
-                            resizeMode={'cover'}
-                            style={{ width: '100%', height: imageHeight }} 
-                            source={require('../images/promotion_3.png')} 
-                        />
-                    </View>
-                </Swiper>
+                <Slick>
+                    <SlickItem source={require('../images/promotion_mockup.png')} />
+                    <SlickItem source={require('../images/promotion_2.png')} />
+                    <SlickItem source={require('../images/promotion_3.png')} />
+                    <SlickItem source={require('../images/promotion3.png')} />
+                </Slick>
                 <Text style={{ flex: 1 }}>Section</Text>
             </ScrollView>
         );
     }
 }
-
-const styles = {
-    wrapper: {
-    },
-    slide1: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#9DD6EB',
-    },
-    slide2: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#97CAE5',
-    },
-    slide3: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#92BBD9',
-    },
-    text: {
-        color: '#fff',
-        fontSize: 30,
-        fontWeight: 'bold',
-    }
-};
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -8,3 +8,5 @@ export * from './AuthCard';
 export * from './IconTab';
 export * from './CardSection';
 export * from './ButtonSideList';
+export * from './Slick';
+export * from './SlickItem';
