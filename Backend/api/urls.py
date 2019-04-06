6ccf6c72ca83c2873362acd7b8ce1eaf8729fd51
Sivakorn Supumporn <sivakornterk@gmail.com>
2019-04-07 02:15:35 +0700

    Done CustomerHistoryAPI

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from account.views import GetCustomerData
+from customer.views import CustomerHistoryAPIView
 
 from .views import *
 
 urlpatterns = [
     path('home', HomeAPIView.as_view()),
     path('get-customer-data',GetCustomerData.as_view()),
+    path('get-customer-history',CustomerHistoryAPIView.as_view()),
     # path('test', TestAPIView.as_view()),
 ]
