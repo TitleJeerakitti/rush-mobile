f4cb06b02b02519ffb188bef80d2794a62f201d8
Sivakorn Supumporn <sivakornterk@gmail.com>
2019-04-19 17:33:45 +0700

    fix script

--- Backend/scripts/init__supplier_timestamp.py
+++ Backend/scripts/init__supplier_timestamp.py
@@ -4,8 +4,8 @@ from report.models import *
 import datetime
 
 def run():
-    time = datetime.datetime.strptime('2019-04-05', '%Y-%m-%d')
-    supplier_list = Supplier.objects.all()
+    time = datetime.datetime.strptime('2017-04-05', '%Y-%m-%d')
+    supplier_list = Supplier.objects.filter(user__id=1)
     for supplier in supplier_list:
         supplier.timestamp = time
         supplier.save()
